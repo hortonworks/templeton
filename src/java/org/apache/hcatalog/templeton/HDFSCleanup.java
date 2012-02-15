e9cff70a44721b69da2ebdce98271825eaa57c0d
Rachel Gollub <rachel@gollub.net>
2012-02-15 12:56:24 -0800

    Added HDFS storage code.

---
+++ src/java/org/apache/hcatalog/templeton/HDFSCleanup.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hcatalog.templeton;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hcatalog.templeton.TempletonStorage.Type;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This does periodic cleanup
+ */
+public class HDFSCleanup extends Thread {
+    protected AppConfig appConf;
+
+    // The interval to wake up and check the queue
+    public static final String HDFS_CLEANUP_INTERVAL =
+        "templeton.zookeeper.cleanup.interval"; // 12 hours
+
+    // The max age of a task allowed
+    public static final String HDFS_CLEANUP_MAX_AGE =
+        "templeton.zookeeper.cleanup.maxage"; // ~ 1 week
+
+    protected static long interval = 1000L * 60L * 60L * 12L;
+    protected static long maxage = 1000L * 60L * 60L * 24L * 7L;
+
+    // The logger
+    private static final Log LOG = LogFactory.getLog(HDFSCleanup.class);
+
+    // Handle to cancel loop
+    private boolean stop = false;
+
+    // The instance
+    private static HDFSCleanup thisclass = null;
+
+    // Whether the cycle is running
+    private static boolean isRunning = false;
+    
+    /**
+     * Create a cleanup object. 
+     */
+    private HDFSCleanup(AppConfig appConf) {
+        this.appConf = appConf;
+        interval = appConf.getLong(HDFS_CLEANUP_INTERVAL, interval);
+        maxage = appConf.getLong(HDFS_CLEANUP_MAX_AGE, maxage);
+    }
+
+    public static HDFSCleanup getInstance(AppConfig appConf) {
+        if (thisclass != null) {
+            return thisclass;
+        }
+        thisclass = new HDFSCleanup(appConf);
+        return thisclass;
+    }
+
+    public static void startInstance(AppConfig appConf) throws IOException {
+        if (!isRunning) {
+            getInstance(appConf).start();
+        }
+    }
+
+    /**
+     * Run the cleanup loop.
+     *
+     */
+    public void run() {
+    	FileSystem fs = null;
+        while (!stop) {
+            try {
+                // Put each check in a separate try/catch, so if that particular
+                // cycle fails, it'll try again on the next cycle.
+                try {
+        			fs = FileSystem.get(appConf);
+        			checkFiles(fs);
+                } catch (Exception e) {
+                    LOG.error("Cleanup cycle failed: " + e.getMessage());
+                } finally {
+                    if (fs != null) {
+                        try {
+                            fs.close();
+                            fs = null;
+                        } catch (Exception e) {
+                            // We're trying to exit anyway, just ignore.
+                        }
+                    }
+                }
+
+                long sleepMillis = (long) (Math.random() * interval);
+                LOG.info("Next execution: " + new Date(new Date().getTime()
+                                                       + sleepMillis));
+                Thread.sleep(sleepMillis);
+
+            } catch (Exception e) {
+                // If sleep fails, we should exit now before things get worse.
+                isRunning = false;
+                LOG.error("Cleanup failed: " + e.getMessage(), e);
+            }
+        }
+        isRunning = false;
+    }
+    
+    /**
+     * Loop through all the files, deleting any that are older than
+     * maxage.
+     * 
+     * @param fs
+     * @throws IOException
+     */
+    private void checkFiles(FileSystem fs) throws IOException {
+    	long now = new Date().getTime();
+	    for (Type type : Type.values()) {
+			for (FileStatus status : fs.listStatus(new Path(
+						HDFSStorage.getPath(type)))) {
+				if (now - status.getModificationTime() > maxage) {
+					fs.delete(status.getPath(), false);
+				}
+					
+			}
+		}
+    }
+
+    // Handle to stop this process from the outside if needed.
+    public void exit() {
+        stop = true;
+    }
+
+    public static void main(String[] args) {
+        try {
+            HDFSCleanup.startInstance(AppConfig.getInstance());
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+    }
+
+}
