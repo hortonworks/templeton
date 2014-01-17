8b4c591cdb1817de885416fb99bf72fcf6361cac
Deepesh Khandelwal <deepesh@hortonworks.com>
2014-01-17 14:24:17 -0800

    Port HIVE-6211 to robustify the test

--- src/test/e2e/templeton/drivers/TestDriverCurl.pm
+++ src/test/e2e/templeton/drivers/TestDriverCurl.pm
@@ -608,20 +608,25 @@ sub compare
       foreach my $key (sort keys %$json_matches) {
         my $regex_expected_value = $json_matches->{$key};
         my $path = JSON::Path->new($key);
-        my $value; 
-        # when check_templeton_queue_name is defined 
-        if (defined $testCmd->{'check_templeton_queue_name'}) {
-	        # decode $testResult->{'body'} to an array of hash
-	        my $body = decode_json $testResult->{'body'};
-	        # in the tests, we run this case with jobName = "TempletonControllerJob"
-	        # filter $body to leave only records with this jobName
-	        my @filtered_body = grep {($_->{detail}{profile}{jobName} eq "TempletonControllerJob")}  @$body;
-			my @sorted_filtered_body = sort { $a->{id} cmp $b->{id} } @filtered_body;
-        	$value = $path->value(\@sorted_filtered_body);
+        # decode $testResult->{'body'} to an array of hash
+        my $body = decode_json $testResult->{'body'};
+        my @filtered_body;
+        if (defined $testCmd->{'filter_job_names'}) {
+          foreach my $filter (@{$testCmd->{'filter_job_names'}}) {
+            my @filtered_body_tmp = grep { $_->{detail}{profile}{jobName} eq $filter } @$body;
+            @filtered_body = (@filtered_body, @filtered_body_tmp);
+          }
         } else {
-        	$value = $path->value($testResult->{'body'});
+          @filtered_body = @$body;
         }
-        
+        my @sorted_filtered_body;
+        if (ref @$body[0] eq 'HASH') {
+          @sorted_filtered_body = sort { $a->{id} cmp $b->{id} } @filtered_body;
+        } else {
+          @sorted_filtered_body = sort { $a cmp $b } @filtered_body;
+        }
+        my $value = $path->value(\@sorted_filtered_body);
+
         if ($value !~ /$regex_expected_value/s) {
           print $log "$0::$subName INFO check failed:"
             . " json pattern check failed. For field "
